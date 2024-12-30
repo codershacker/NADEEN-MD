766d8e6f2ba66736083d31a444219169b8553f28
codershacker <manhirunadeen@gmail.com>
2024-12-30 11:45:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "BD93naLB#ZOJpgLrI7GZKGHJa1d9bGjCtlk1NH48smC5Rt99XmW8", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
-AUTO_VOICE:"true" //true or false
+AUTO_VOICE: "true" //true or false
 };
